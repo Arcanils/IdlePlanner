105b2cde3fd54bacf74befa6d920ad0b720a67e8
Petiot Thibaut <petiotthib@gmail.com>
2018-05-15 23:49:42 +0200

    Factory robot inprogress Controller IA base in progress MC + view optionel in progress

---
+++ SRC/Assets/Scripts/FactoryRobots.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FactoryRobots : MonoBehaviour {
+
+	public GameObject PrefabPawn;
+
+	private IMapData _iMapData;
+
+	public void Init(IMapData iMapData)
+	{
+		_iMapData = iMapData;
+	}
+
+	public void CreateEntity(Vector2Int positionCell)
+	{
+		var logic = CreateEntityLogic(positionCell);
+		var controller = CreateController(logic);
+		var pawn = CreatePawn();
+	}
+
+	public EntityView CreatePawn()
+	{
+		return GameObject.Instantiate(PrefabPawn).GetComponent<EntityView>();
+	}
+
+	public EntityModel CreateEntityLogic(Vector2Int positionCell)
+	{
+		return new EntityModel(_iMapData, positionCell);
+	}
+
+	public EntityController CreateController(EntityModel model)
+	{
+		return new EntityController(model);
+	}
+
+}
--- SRC/Assets/Scripts/PawnComponent.cs
+++ SRC/Assets/Scripts/PawnComponent.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PawnComponent : MonoBehaviour {
-	
+public class EntityView : MonoBehaviour {
+
+	private EntityModel _data;
+
+	public void Init(EntityModel data)
+	{
+		_data = data;
+	}
+
 	public void Move()
 	{
 
@@ -25,8 +32,18 @@ public class PawnComponent : MonoBehaviour {
 	}
 }
 
-public class EntityData
+public class EntityModel : IEntityModel
 {
+	public enum EStateModel
+	{
+		SPAWN,
+		MOVE,
+		SCAN,
+		COLLECT,
+		BACK,
+		OTHERS,
+	}
+
 	public int RangeVision = 1;
 	public int RangeScanVision = 3;
 	public int CapacityStorage = 5;
@@ -35,16 +52,22 @@ public class EntityData
 	public Vector2Int Position { get; private set; }
 	public Vector2Int PrevPosition { get; private set; }
 	public int AmountCollected { get; private set; }
+	public EStateModel State { get; private set; }
 
 	private ETile[,] _visionMap;
 	private IMapData _iMapData;
+	private EntityView _pawn;
 
-	public EntityData(IMapData iMapData, ETile[,] visionMap, Vector2Int startPosition)
+	public EntityModel(IMapData iMapData, Vector2Int startPosition)
 	{
-
+		_iMapData = iMapData;
+		_visionMap = iMapData.GetMap();
+		Position = startPosition;
+		PrevPosition = startPosition;
+		State = EStateModel.SPAWN;
 	}
 
-	public void Collect()
+	void IEntityModel.Collect()
 	{
 		if (AmountCollected >= CapacityStorage)
 			return;
@@ -55,9 +78,10 @@ public class EntityData
 			_iMapData.UpdateMap(ref _visionMap, Position);
 		}
 
+		State = EStateModel.COLLECT;
 	}
 
-	public void MoveTo(Vector2Int newPosition)
+	void IEntityModel.MoveTo(Vector2Int newPosition)
 	{
 		if (CurrentAmountFuel <= 0)
 			return;
@@ -65,10 +89,14 @@ public class EntityData
 		Position = newPosition;
 		--CurrentAmountFuel;
 		_iMapData.UpdateMap(ref _visionMap, GetPositionToUpdate(RangeVision, Position));
+
+		State = EStateModel.MOVE;
 	}
-	public void Scan()
+	void IEntityModel.Scan()
 	{
 		_iMapData.UpdateMap(ref _visionMap, GetPositionToUpdate(RangeScanVision, Position));
+
+		State = EStateModel.SCAN;
 	}
 
 	private static Vector2Int[] GetPositionToUpdate(int rangeScan, Vector2Int position)
@@ -88,6 +116,7 @@ public class EntityData
 
 public interface IMapData
 {
+	ETile[,] GetMap();
 	void UpdateMap(ref ETile[,] visionMap, params Vector2Int[] tilesToUpdate);
 	bool Collect(Vector2Int position);
 }
@@ -106,6 +135,8 @@ public enum EActionCondition
 	COLLECT,
 	SCAN,
 	FIND_NEW_PATH,
+	MOVE_FORWARD,
+	BACK,
 }
 
 public enum ECondition
@@ -125,4 +156,78 @@ public struct TileMap
 {
 	public ETile Type;
 	public int Amount;
+}
+
+public class EntityController
+{
+	private IEntityModel _actionModel;
+	private AIBehaviour _ai;
+
+	public EntityController(IEntityModel actionModel)
+	{
+		_actionModel = actionModel;
+	}
+
+	public void Tick()
+	{
+		for (int i = 0; i < _ai.Gambits.Count; i++)
+		{
+			var gambit = _ai.Gambits[i];
+			if (EvaluateCondtion(_actionModel, gambit.Condition, gambit.Target))
+			{
+				ExecuteAction(_actionModel, gambit.Action);
+				break;
+			}
+		}
+
+		ExecuteAction(_actionModel, EActionCondition.BACK);
+	}
+
+	private static bool EvaluateCondtion(IEntityModel model, ECondition condition, ETargetCondition target)
+	{
+		return true;
+	}
+
+	private static void ExecuteAction(IEntityModel model, EActionCondition action)
+	{
+		switch (action)
+		{
+			case EActionCondition.COLLECT:
+				model.Collect();
+				break;
+			case EActionCondition.SCAN:
+				model.Scan();
+				break;
+			case EActionCondition.FIND_NEW_PATH:
+				//A* new path
+				break;
+			case EActionCondition.MOVE_FORWARD:
+				// Move forward
+				break;
+			case EActionCondition.BACK:
+				// Finish
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+public struct AIGambitLine
+{
+	public ECondition Condition;
+	public ETargetCondition Target;
+	public EActionCondition Action;
+}
+
+public class AIBehaviour
+{
+	public List<AIGambitLine> Gambits;
+}
+
+public interface IEntityModel
+{
+	void Collect();
+	void MoveTo(Vector2Int newPosition);
+	void Scan();
 }
